aff13ce297f93d32479ca73d610e1905d25ba67c
Alessandro Pellegrini <pellegrini@dis.uniroma1.it>
2015-07-15 10:35:44 +0000

    a

--- controller/balancer.c
+++ controller/balancer.c
@@ -24,7 +24,7 @@
 
 pthread_mutex_t mutex;
 int res_thread;
-int lambda = 0;
+int lambda;
 timer update_local_region_features_timer;
 char my_own_ip[16];
 int port_remote_balancer;
@@ -195,7 +195,7 @@ void *update_region_features(void * sock) {
 		}
 		printf("-----------------\n");
 		timer_restart(update_local_region_features_timer);
-		printf("User request arrival rate: %d, interval: %d\n", local_region_user_request_arrival_rate, UPDATE_LOCAL_REGION_FEATURE_INTERVAL);
+		printf("User request arrival rate: %f, interval: %d\n", local_region_user_request_arrival_rate, UPDATE_LOCAL_REGION_FEATURE_INTERVAL);
 		pthread_mutex_lock(&mutex);
 		lambda = 0;
 		pthread_mutex_unlock(&mutex);
@@ -266,13 +266,10 @@ void *client_sock_id_thread(void *vm_client_arg) {
 	if (!vm_client.user_type) {
         	pthread_mutex_lock(&mutex);
                 lambda++;
-		printf("Current lambda: %i\n");	
                 pthread_mutex_unlock(&mutex);
 	}
 	// We never finish forwarding data!
 	while (1) {
-		if (!vm_client.user_type)
-                	lambda++;
 		int listnum; //Current item in connectlist for for loops
 		/* First put together fd_set for select(), which will
 		 consist of the sock veriable in case a new client_sock_id
@@ -553,6 +550,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	vm_list = NULL;
+	lambda=0;
 	port_update_region_features = atoi(argv[4]);
 	port_remote_balancer = atoi(argv[6]);
 	/* CONNECTION LB - CONTROLLER */
@@ -706,7 +704,6 @@ int main(int argc, char *argv[]) {
 		strcpy(vm_client->ip_address, inet_ntoa(client.sin_addr));
 		vm_client->port = ntohs(client.sin_port);
 		vm_client->user_type = 0;
-		lambda++;
 		//printf("Creating new thread for client <%s, %d>\n", vm_client->ip_address, vm_client->port);
 		//fflush(stdout);
 		res_thread = create_thread(client_sock_id_thread, vm_client);
