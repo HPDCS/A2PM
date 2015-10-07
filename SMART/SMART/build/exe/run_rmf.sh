39f9827828a903a0faeff80621fab6743840cb40
Alessandro Pellegrini <pellegrini@dis.uniroma1.it>
2015-10-07 13:10:14 +0000

    w

--- SMART/SMART/build/exe/run_rmf.sh
+++ SMART/SMART/build/exe/run_rmf.sh
@@ -3,9 +3,11 @@ rm nohup.out
 echo "Launching router ..."
 pkill router
 nohup ./router exhaustive 1 &
+sleep 2
 echo "Launching forwarder ..."
 pkill forwarder
 nohup ./forwarder 121.0.0.1 &
+sleep 2
 echo "Launching monitor ..."
 pkill monitor
 nohup ./monitor 127.0.0.1 &
