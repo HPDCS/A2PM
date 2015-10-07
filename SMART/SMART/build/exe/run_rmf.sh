1e35d8f0e8bb6dd600498708f59c18c4ac2c88f1
Alessandro Pellegrini <pellegrini@dis.uniroma1.it>
2015-10-07 13:23:37 +0000

    w

--- SMART/SMART/build/exe/run_rmf.sh
+++ SMART/SMART/build/exe/run_rmf.sh
@@ -2,12 +2,13 @@
 rm nohup.out
 echo "Launching router ..."
 pkill router
-nohup ./router exhaustive 1 &
-sleep 2
-echo "Launching forwarder ..."
-pkill forwarder
-nohup ./forwarder 121.0.0.1 &
+nohup ./router exhaustive 1 > router_output.log &
 sleep 2
 echo "Launching monitor ..."
 pkill monitor
-nohup ./monitor 127.0.0.1 &
+nohup ./monitor 127.0.0.1 > monitor_output.log &
+sleep 2
+echo "Launching forwarder ..."
+pkill forwarder
+nohup ./forwarder 127.0.0.1 > forwarder_output.log &
+
