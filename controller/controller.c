3684291439a00a8ee700f53353d3f1fdadf6fd71
io <disanzo@dis.uniroma1.it>
2016-01-25 15:03:44 +0100

    a

--- controller/controller.c
+++ controller/controller.c
@@ -358,7 +358,8 @@ void lb_function_3() {
 		if (strnlen(regions[index].ip_controller, 16) != 0
 				&& !isnan(regions[index].region_features.mttf)) {
 			if (regions[index].region_features.mttf<average_rmttf) {
-				float new_prob=regions[index].probability*(regions[index].region_features.mttf/average_rmttf);
+				float new_prob=regions[index].probability -
+						(1/2)*regions[index].probability *(1- regions[index].region_features.mttf/average_rmttf);
 				printf("\tNew forwarding probability for region %i: %f,", index, new_prob);
 				total_prob_reduction=total_prob_reduction+(regions[index].probability-new_prob);
 				regions[index].probability=new_prob;
